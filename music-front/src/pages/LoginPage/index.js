709d85a30e914057baa7eb24d0fa6b9a6b4a4220
pze <pze@pop-os.localdomain>
2022-04-27 16:47:30 -0300

    teste

--- music-front/src/pages/LoginPage/index.js
+++ music-front/src/pages/LoginPage/index.js
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 import { Container } from "./styles";
 
 const Login = () => {
+
     return (
         
             <Container>
+
                 <h1>Entrar com uma conta</h1>
                 <input class="login" placeholder="USERNAME"></input>
                 <input class="login" type="password" placeholder="PASSWORD"></input>
