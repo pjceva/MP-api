00fc8a155bef894ad124795ceb0fbb4c6c77e3d0
GabrielCMoretto <gabrielmorettopvh@yahoo.com.br>
2022-05-01 17:54:20 -0300

    Um commit de teste

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1>#Projeto de disciplina da matéria Métodos de Programação <h1>
+#Projeto de disciplina da matéria Métodos de Programação <h1>
 
 Pedro José Monteiro de Barros Ceva Rodrigues 190139315
 Gabriel Carvalho Moretto 150154917
--- music-api/db/schema.rb
+++ music-api/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,53 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_425_004_826) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_25_004826) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'artists', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'genres', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'likes', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'music_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['music_id'], name: 'index_likes_on_music_id'
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "music_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["music_id"], name: "index_likes_on_music_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'musics', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'likes'
-    t.bigint 'artist_id', null: false
-    t.bigint 'genre_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'mainartist'
-    t.index ['artist_id'], name: 'index_musics_on_artist_id'
-    t.index ['genre_id'], name: 'index_musics_on_genre_id'
+  create_table "musics", force: :cascade do |t|
+    t.string "name"
+    t.integer "likes"
+    t.bigint "artist_id", null: false
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "mainartist"
+    t.index ["artist_id"], name: "index_musics_on_artist_id"
+    t.index ["genre_id"], name: "index_musics_on_genre_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'password'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'is_admin'
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "is_admin"
   end
 
-  add_foreign_key 'likes', 'musics'
-  add_foreign_key 'likes', 'users'
-  add_foreign_key 'musics', 'artists'
-  add_foreign_key 'musics', 'genres'
+  add_foreign_key "likes", "musics"
+  add_foreign_key "likes", "users"
+  add_foreign_key "musics", "artists"
+  add_foreign_key "musics", "genres"
 end
