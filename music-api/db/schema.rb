70bbbc40effef1a0849fc5ea95b8a95c1a2ea376
Gabriel Henrique Souza de Melo <42751354+gabriel-smello@users.noreply.github.com>
2022-05-01 20:07:15 -0300

    pre merge

--- music-api/db/schema.rb
+++ music-api/db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_25_004826) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "is_admin"
